3c479288681ba57cbda985a8fca3af5543c877a4
장정안 <wjddks96@naver.com>
2023-09-25 13:12:41 +0900

    feat: radixUI 세부내역 설치 및 트리구조 라이브러리 설치 등 기타 작업에 필요한 것들 설치

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import CodeEditor from "./codeEditor/CodeEditor";
+import HeaderMenubar from "./ideUI/header/HeaderMenubar";
+import SideCollapsible from "./ideUI/sideBar/SideCollapsible";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderMenubar />
+      <Layout />
     </div>
   );
 }
