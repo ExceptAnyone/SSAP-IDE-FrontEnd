c2982da704b6818720f777fb3cf0a0ed1ab783d6
haeun jeon <yul__koo@naver.com>
2023-10-06 22:17:31 +0900

    design:버튼의 링크 css 수정

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import BackButton from "./BackButton";
 import ProfileIcon from "./ProfileIcon";
 import "./Header.css"; // 헤더 스타일링을 위한 CSS 파일
+import { Link } from "react-router-dom";
 
 export default function Header(props) {
   const password = props.password;
@@ -18,20 +19,24 @@ export default function Header(props) {
           <div className="profileicon-right">
             <ProfileIcon />
           </div>
+        </div>
+        <Link style={{ textDecoration: "none" }} to={props.link}>
           <button
             className="edit-btn"
             disabled={
               password !== confirmPassword ||
               (!password && !containnername && !MainPage)
             }
+            onClick={props.updateContainer}
           >
             {props.name}
           </button>
-          {/* isValidPassword 값에 따라 버튼의 disabled 속성을 설정하여 활성화/비활성화 조절 */}
-        </div>
-        <div className="header-bottom">
+        </Link>
+        {/* isValidPassword 값에 따라 버튼의 disabled 속성을 설정하여 활성화/비활성화 조절 */}
+
+        <h3>
           <BackButton icon={props.icon} />
-        </div>
+        </h3>
       </div>
 
       <hr />
