46a975275be0f700ab775b082b7c4b6fd9f6e228
Seongsuk Hong <ssukh77@naver.com>
2023-10-06 12:44:11 +0900

    refactor(front폴더 밖에 불필요한 파일 정리2): #56

--- front/src/api/chatAPI.js
+++ front/src/api/chatAPI.js
@@ -1,3 +1,3 @@
 // export const API_ENDPOINT = "http://localhost:5012";
 export const API_ENDPOINT =
-  "http://Chat-env.eba-pbveipzf.ap-northeast-2.elasticbeanstalk.com/";
+  "http://Chat-env.eba-pbveipzf.ap-northeast-2.elasticbeanstalk.com/chat";
