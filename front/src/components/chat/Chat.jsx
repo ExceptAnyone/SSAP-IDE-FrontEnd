9cdafd13c33b010d1b5cba53778c784974940b10
Seongsuk Hong <ssukh77@naver.com>
2023-10-08 22:26:17 +0900

    feat(채팅 데이터 불러와서 처리 로직 수정): #63
    - react-query 설치 및 설정:
    비동기 통신을 위해 react-query 설치와 index.js에 기본 설정을 했습니다.
    
    - useQuery로 roomId를 매개변수로 받아 getMessages 함수 실행해서 API 호출:
    useQuery로 roomId를 매개변수로 받아 getMessages 함수를 실행해서 서버에 API를 호출하였습니다. roomId가 존재할 때만 enabled에 쿼리를 활성화하도록 하고 쿼리가 성공적으로 데이터를 가져온 경우 onSuccess에 데이터를 setMessages(data) 저장하도록 설정 했습니다. 또한 쿼리가 실패할 경우 에러 메세지 남용 방지를 위해 일단 retry를 최대 3번의 재시도를 설정했고 retryDelay로 재시도 사이의 지연 시간을 설정했는데 추후 확인하고 삭제 여부를 결정하도록 하겠습니다.
    
    - test 사용자 입력받는 prompt 부분, joinTime (접속 시간) 삭제
    
    - <IdePage />에서 내려받은 사용자 email, name 데이터 소켓 setUserInfo로 전송:
    IdePage 컴포넌트에서 내려받은 사용자 email, name 데이터를 추가하면서 서버에 보내야하는 데이터에 따라 데이터 값이 수정이 되었고 소켓 setUserInfo로 전송 로직을 추가 하였습니다.

--- front/src/components/chat/Chat.jsx
+++ front/src/components/chat/Chat.jsx
@@ -1,30 +1,38 @@
 import React, { useState, useEffect, useRef } from "react";
 import socketIOClient from "socket.io-client";
-import { API_ENDPOINT } from "../../api/chatAPI";
+import { API_ENDPOINT, getMessages } from "../../api/chatAPI";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import { BiSearch, BiSolidUser, BiSolidBell } from "react-icons/bi";
 import { BsChatSquareDots } from "react-icons/bs";
 import "./chat.scss";
 import ChatUserList from "./chatUserList/ChatUserList";
+import { useQuery } from "@tanstack/react-query";
 
-const Chat = ({ roomId }) => {
-  const [userName, setUserName] = useState("");
+const Chat = ({ roomId, name, email }) => {
+  // const [name, setName] = useState(name);
   const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState("");
+  const [message, setMessage] = useState("");
   const socketRef = useRef();
 
   const [isVisible, setIsVisible] = useState(false);
   const [newMessageArrived, setNewMessageArrived] = useState(false);
 
-  // TEST: 페이지 접속시 사용자 입력 받기
-  // TODO: 추후 유저 정보 받아서 작업 예정
-  useEffect(() => {
-    if (!userName) {
-      const inputUserName = prompt("이름을 입력해주세요.:");
-      if (inputUserName) setUserName(inputUserName);
-      console.log(inputUserName);
-    }
-  }, []);
+  const {
+    data: chatData,
+    isLoading,
+    isError,
+  } = useQuery(
+    ["getMessages", roomId], // 쿼리 키
+    () => getMessages(roomId), // 데이터를 가져오는 함수
+    {
+      enabled: !!roomId, // roomId가 존재할 때만 활성화
+      onSuccess: (data) => {
+        setMessages(data);
+      },
+      retry: 3, // 실패 시 최대 3번 재시도
+      retryDelay: (attempt) => Math.min(attempt * 1000, 3000), // 재시도 사이의 지연 시간 설정
+    },
+  );
 
   // 채팅창 열림 여부 최신 상태 업뎃 유지
   const isVisibleRef = useRef(isVisible);
@@ -33,41 +41,47 @@ const Chat = ({ roomId }) => {
     console.log("isVisible:", isVisible); // isVisible 상태 확인용
   }, [isVisible]);
 
-  // 클라이언트 접속 시간
-  const joinTime = new Date();
-
   // 소켓
   useEffect(() => {
-    if (!userName) return;
+    if (!email) return;
 
     // Socket.IO 클라이언트 초기화
-    socketRef.current = socketIOClient(API_ENDPOINT, {
+    const socket = socketIOClient(API_ENDPOINT, {
       //cors 체크
       withCredentials: true,
     });
 
-    socketRef.current.emit("setUsername", userName);
-    socketRef.current.emit("joinRoom", roomId, joinTime);
+    socket.emit("setUserInfo", {
+      email: email,
+      name: name,
+    });
+    console.log(email, name);
+    socket.emit("joinRoom", roomId, email);
 
     // 서버로부터 메시지를 받으면 상태 업데이트
-    socketRef.current.on("receiveMessage", (msg) => {
-      // 본인 확인
-      const isOwner = msg.socketId === socketRef.current.id;
+    socket.on("receiveMessage", (data) => {
+      console.log("Received message data: ", data);
 
-      setMessages((prevMessages) => [...prevMessages, msg]);
-      console.log("isOwner:", isOwner, "isVisible:", isVisible);
+      const { name, message } = data;
+      setMessages((prevMessages) => [...prevMessages, { name, message }]);
 
-      // 새로운 메세지 도착하면 알림
-      if (!isOwner && !isVisibleRef.current) {
-        setNewMessageArrived(true);
-      }
+      // TODO: 본인 확인 - 수정 필요
+      // const isOwner = msg.socketId === socketRef.current.id;
+      // console.log("isOwner:", isOwner, "isVisible:", isVisible);
+
+      // TODO: 새로운 메세지 도착하면 알림 - 수정 필요
+      // if (!isOwner && !isVisibleRef.current) {
+      //   setNewMessageArrived(true);
+      // }
     });
+
+    socketRef.current = socket;
 
     // 소켓 연결 종료
     return () => {
       socketRef.current.disconnect();
     };
-  }, [roomId, userName]);
+  }, [roomId, name]);
 
   // 메세지 엡뎃 확인용
   useEffect(() => {
@@ -85,7 +99,6 @@ const Chat = ({ roomId }) => {
     setIsVisible((prevIsVisible) => !prevIsVisible); // isVisible 상태 토글
   };
 
-  // TODO: BUG - 마지막글자 두번 전송 됨 수정 필요
   // textarea 엔터 키 전송
   const handleTextareaKeyPress = (e) => {
     if (e.key === "Enter" && !e.shiftKey) {
@@ -97,16 +110,12 @@ const Chat = ({ roomId }) => {
   // 채팅 메세지 전송
   const handleSendMessage = (e) => {
     if (e) e.preventDefault();
-    if (newMessage.trim() === "") return;
-    const socketId = socketRef.current.id;
-    const messageObject = {
-      userName,
-      content: newMessage,
-    };
+    if (message.trim() === "") return;
 
-    console.log("messageObject", messageObject);
-    socketRef.current.emit("sendMessage", roomId, socketId, messageObject);
-    setNewMessage("");
+    const data = { email, name, message };
+    socketRef.current.emit("sendMessage", roomId, data);
+    console.log("메세지 전송: ", data);
+    setMessage("");
   };
 
   return (
@@ -126,31 +135,31 @@ const Chat = ({ roomId }) => {
               </div>
             </div>
             <div className="msg-wrap">
-              {messages.map((message, index) => (
+              {messages.map((msg, index) => (
                 <div
                   key={index}
                   className={`message ${
-                    message.socketId === socketRef.current.id
+                    msg.socketId === socketRef.current.id
                       ? "msg_right"
                       : "msg_left"
                   }`}
                 >
-                  {message.socketId === socketRef.current.id ? (
+                  {msg.socketId === socketRef.current.id ? (
                     ""
                   ) : (
-                    <strong>{message.userName}</strong>
+                    <strong>{msg.name}</strong>
                   )}
 
-                  <span>{message.content}</span>
+                  <span>{msg.message}</span>
                 </div>
               ))}
             </div>
             <div className="msg-form">
               <form onSubmit={handleSendMessage}>
                 <textarea
-                  value={newMessage}
+                  value={message}
                   autoFocus={true}
-                  onChange={(e) => setNewMessage(e.target.value)}
+                  onChange={(e) => setMessage(e.target.value)}
                   onKeyPress={handleTextareaKeyPress}
                 ></textarea>
                 <button type="submit">전송</button>
--- front/src/index.js
+++ front/src/index.js
@@ -4,12 +4,17 @@ import "./index.css";
 import App from "./App";
 import { store } from "./components/ide/store";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
 );
