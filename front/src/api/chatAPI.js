2d89606f71461108efa2b6c63b25493cbad8e75e
Seongsuk Hong <ssukh77@naver.com>
2023-10-08 20:56:18 +0900

    feat(채팅 메시지 불러오는 API 함수 추가): #61
    채팅 메세지 불러오는 getMessages 함수를 추가하였습니다.
    모듈 충돌 오류로 node 모듈 재설치 진행했습니다.
    
    Fixes : getMessages API 요청 실패 오류 #62

--- front/src/api/chatAPI.js
+++ front/src/api/chatAPI.js
@@ -1,3 +1,20 @@
-// export const API_ENDPOINT = "http://localhost:5012";
-export const API_ENDPOINT =
-  "http://Chat-env.eba-pbveipzf.ap-northeast-2.elasticbeanstalk.com/chat";
+import axios from "axios";
+
+const API_ENDPOINT = "http://localhost:5012";
+// export const API_ENDPOINT =
+//   "http://Chat-env.eba-pbveipzf.ap-northeast-2.elasticbeanstalk.com/chat";
+
+// 메시지 불러오기 API 호출
+const getMessages = async (roomId) => {
+  try {
+    const response = await axios.get(`${API_ENDPOINT}/chat/getMessages`, {
+      params: { roomId },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("API 요청 실패:", error);
+    throw new Error("API 요청을 수행하지 못했습니다");
+  }
+};
+
+export { API_ENDPOINT, getMessages };
