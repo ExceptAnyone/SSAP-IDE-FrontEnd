b93ab2f5b051f762fe6386d8917d1be675acf4f8
장정안 <126966681+ExceptAnyone@users.noreply.github.com>
2023-10-10 00:54:29 +0900

    Revert "3번째 머지"

--- front/src/api/chatAPI.js
+++
@@ -1,3 +0,0 @@
-// export const API_ENDPOINT = "http://localhost:5012";
-export const API_ENDPOINT =
-  "http://Chat-env.eba-pbveipzf.ap-northeast-2.elasticbeanstalk.com/chat";
--- front/src/components/ide/store.jsx
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import fileReducer from "./fileSlice/FileSlice";
-export const store = configureStore({
-  reducer: {
-    file: fileReducer,
-  },
-});
